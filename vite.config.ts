411e25dae8072f31a45e7327856d525b1fa4ecb7
Neil Patrick Mendoza <mendoza@housingjapan.com>
2022-10-15 16:27:31 +0800

    Add shortcut for bootstrap

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { fileURLToPath, URL } from 'node:url';
-
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap')
     }
   }
 });
