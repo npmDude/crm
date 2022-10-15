1c7140c3e11e9f4925a98d90a12f2d49e996fea2
Neil Patrick Mendoza <mendoza@housingjapan.com>
2022-10-15 16:27:59 +0800

    Add scss file

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
-import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { createApp, markRaw } from 'vue';
 
 import App from './App.vue';
 import router from './router';
 
-import './assets/main.css';
+// CSS
+import './assets/main.scss';
+
+// JS
+import 'bootstrap';
 
 const app = createApp(App);
 
-app.use(createPinia());
+const pinia = createPinia();
+pinia.use(({ store }) => {
+  store.router = markRaw(router);
+});
+
+app.use(pinia);
 app.use(router);
 
 app.mount('#app');
