c2a69a3a9347292d2675571456e536285e5a112c
Neil Patrick Mendoza <mendoza@housingjapan.com>
2022-10-15 20:10:54 +0800

    Add no data table row component

---
+++ src/components/NoDataTableRow.vue
@@ -0,0 +1,18 @@
+<template>
+  <tr>
+    <td :colspan="colspan" class="text-center">
+      {{ message }}
+    </td>
+  </tr>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  message?: string;
+  colspan?: number;
+}
+
+withDefaults(defineProps<Props>(), {
+  message: 'No data to display.'
+});
+</script>
