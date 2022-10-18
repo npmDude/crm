d4d204fc2aefc6de41ce71d2939a66f18a16138d
Neil Patrick Mendoza <npm.neil@gmail.com>
2022-10-19 01:32:12 +0800

    Rename validity to valid

--- src/composables/useFieldValidity.ts
+++ src/composables/useFieldValidity.ts
@@ -2,19 +2,19 @@ import type { FieldMeta } from 'vee-validate';
 import { computed } from 'vue';
 
 export default function useFieldValidity(fieldMeta: FieldMeta<unknown>) {
-  const validity = computed(() =>
+  const valid = computed(() =>
     fieldMeta.dirty || fieldMeta.validated ? fieldMeta.valid : null
   );
 
   const validityClass = computed(() => {
-    if (validity.value) {
+    if (valid.value) {
       return 'is-valid';
-    } else if (validity.value === false) {
+    } else if (valid.value === false) {
       return 'is-valid';
     }
 
     return null;
   });
 
-  return { validityClass };
+  return { valid, validityClass };
 }
