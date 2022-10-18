ff0693bb7850668b34091c3acec83c1dde4cdd64
Neil Patrick Mendoza <npm.neil@gmail.com>
2022-10-19 01:31:30 +0800

    Update useFieldValidity

--- src/composables/useFieldValidity.ts
+++ src/composables/useFieldValidity.ts
@@ -2,13 +2,19 @@ import type { FieldMeta } from 'vee-validate';
 import { computed } from 'vue';
 
 export default function useFieldValidity(fieldMeta: FieldMeta<unknown>) {
-  const validity = computed(() => {
-    if (fieldMeta.dirty || fieldMeta.validated) {
-      return fieldMeta.valid ? 'is-valid' : 'is-invalid';
+  const validity = computed(() =>
+    fieldMeta.dirty || fieldMeta.validated ? fieldMeta.valid : null
+  );
+
+  const validityClass = computed(() => {
+    if (validity.value) {
+      return 'is-valid';
+    } else if (validity.value === false) {
+      return 'is-valid';
     }
 
     return null;
   });
 
-  return { validity };
+  return { validityClass };
 }
