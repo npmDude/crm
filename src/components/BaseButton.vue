9c512c1d3c64d2f3f77bec20378cc42bd5cd83c5
Neil Patrick Mendoza <npm.neil@gmail.com>
2022-10-19 13:25:54 +0800

    Add BaseButton component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <button
+    type="button"
+    :class="['btn', `btn-${variant}`, 'position-relative']"
+    :disabled="busy"
+  >
+    <div v-if="busy" class="position-absolute top-50 start-50 translate-middle">
+      <div
+        class="spinner-border spinner-border-sm"
+        role="status"
+        aria-hidden="true"
+      >
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  variant?:
+    | 'primary'
+    | 'secondary'
+    | 'success'
+    | 'danger'
+    | 'warning'
+    | 'info'
+    | 'light'
+    | 'dark'
+    | 'link';
+  busy?: boolean;
+}
+
+withDefaults(defineProps<Props>(), {
+  variant: 'secondary',
+  busy: false
+});
+</script>
