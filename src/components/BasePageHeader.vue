168593a6a88edfa7f310045de4c84d58429d8c15
Neil Patrick Mendoza <npm.neil@gmail.com>
2022-10-20 09:39:51 +0800

    Add base page header component

---
+++ src/components/BasePageHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="hasActions" class="d-flex">
+    <h1 class="mb-0"><slot></slot></h1>
+
+    <div class="d-flex align-items-end ms-auto">
+      <slot name="actions"></slot>
+    </div>
+  </div>
+
+  <h1 v-else class="mb-0"><slot></slot></h1>
+
+  <hr />
+</template>
+
+<script setup lang="ts">
+import { computed, useSlots } from 'vue';
+
+const slots = useSlots();
+const hasActions = computed(() => !!slots.actions);
+</script>
