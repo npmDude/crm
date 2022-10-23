807cf7f9ebe1fb684b127f36c896e9fa0c53a531
Neil Patrick Mendoza <npm.neil@gmail.com>
2022-10-23 16:39:51 +0800

    Add TextPlaceholder component

---
+++ src/components/TextPlaceholder.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-if="busy" class="placeholder-glow">
+    <span :class="`placeholder col-${col}`"></span>
+  </div>
+
+  <slot v-else></slot>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  col?: number;
+  busy?: boolean;
+}
+
+withDefaults(defineProps<Props>(), {
+  col: 12,
+  busy: false
+});
+</script>
